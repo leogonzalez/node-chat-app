7513082ca941b7e05c5a468f85c147c4f2007ff3
Leo Gonzalez <leo.lujan.gonzalez@gmail.com>
2018-04-29 02:10:27 -0300

    mustache complete

--- public/index.js
+++ public/index.js
@@ -19,20 +19,25 @@ socket.on("newMessage", function(msg) {
     createdAt: formattedTime
   });
   jQuery("#messages").append(html)
-
-  // var li = jQuery("<li></li>");
-  // li.text(`[${formattedTime}] ${msg.from}: ${msg.text}`);
-  // jQuery("#messages").append(li);
 });
 
 socket.on("newLocationMessage", function(msg) {
   var formattedTime = moment(msg.createdAt).format("h:mm a");
-  var li = jQuery("<li></li>");
-  var a = jQuery('<a target="_blank">Current Location</a>');
-  li.text(`[${formattedTime}] ${msg.from}: `);
-  a.attr("href", msg.url);
-  li.append(a);
-  jQuery("#messages").append(li);
+  var template = jQuery("#location-message-template").html();
+  var html = Mustache.render(template, {
+    text: msg.text,
+    from: msg.from,
+    url: msg.url,
+    createdAt: formattedTime
+  });
+  jQuery("#messages").append(html)
+  //
+  // var li = jQuery("<li></li>");
+  // var a = jQuery('<a target="_blank">Current Location</a>');
+  // li.text(`[${formattedTime}] ${msg.from}: `);
+  // a.attr("href", msg.url);
+  // li.append(a);
+  // jQuery("#messages").append(li);
 });
 
 jQuery("#message-form").on("submit", function(e) {
